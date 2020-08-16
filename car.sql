88b57e6637389bae074cbb2badf7a4f8481eaff5
shrey82 <38159468+shrey82@users.noreply.github.com>
2020-08-16 19:58:03 +0530

    Update car.sql

--- car.sql
+++ car.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 09, 2017 at 02:14 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
